04d37986c3b069833b95549c330d94ada5e1af40
Bob Wallis <bob.wallis@gmail.com>
2021-03-14 13:59:00 +0000

    Open and export buttons

--- src/js/app.js
+++ src/js/app.js
@@ -54,18 +54,23 @@ document.getElementById( 'input' ).addEventListener( 'click', function() {
 
 
 // Load up web worker
-var gsirilWorker = new Worker( 'gsiril.worker.js' );
-gsirilWorker.onmessage = function( e ) {
-    if(typeof e.data.output == 'string' ) {
-        document.getElementById('sirilOutput').innerHTML += e.data.output + "\n";
-    }
-    else if(typeof e.data.error === 'string') {
-        document.getElementById('sirilOutput').innerHTML += '<span class="error">' + e.data.error + "</span>\n"
-    }
-    else {
-        console.log(e.data);
-    }
-};
+try {
+    var gsirilWorker = new Worker( 'gsiril.worker.js' );
+    gsirilWorker.onmessage = function( e ) {
+        if(typeof e.data.output == 'string' ) {
+            document.getElementById('sirilOutput').innerHTML += e.data.output + "\n";
+        }
+        else if(typeof e.data.error === 'string') {
+            document.getElementById('sirilOutput').innerHTML += '<span class="error">' + e.data.error + "</span>\n";
+        }
+        else {
+            console.log(e.data);
+        }
+    };
+} catch( e ) {
+    document.getElementById('sirilOutput').innerHTML = "<p class='placeholder'>Type or paste code into the editor to the left,<br />or open a file from your device.</p><p><span class='error'>GSiril failed to load. The editor will work\nbut you won\'t be able to run the prover.</span></p>";
+    document.getElementById( 'go' ).style.display = 'none';
+}
 
 // Handle click on Prove button
 document.getElementById( 'go' ).addEventListener( 'click', function() {
@@ -88,4 +93,37 @@ document.getElementById( 'aboutButton' ).addEventListener( 'click', function() {
 document.getElementById( 'overlay' ).addEventListener( 'click', function() {
     document.getElementById( 'overlay' ).className = 'hide';
     document.getElementById( 'about' ).className   = 'hide';
+} );
+
+// Handle click on the file open button
+var openFile = document.getElementById( 'openFile' );
+document.getElementById( 'openButton' ).addEventListener( 'click', function() {
+    openFile.click();
+} );
+openFile.addEventListener( 'change', function( e ) {
+    var reader = new FileReader();
+    reader.onload = function( loaded ) {
+        var transaction = view.state.update( { changes: { from: 0, to: view.state.doc.length, insert: loaded.target.result } } );
+        view.update( [transaction] );
+    };
+    reader.onerror = function() {
+        alert( "Failed to read file.\n" + reader.error );
+        reader.abort();
+    }
+    reader.readAsText( e.target.files[0] );
+} );
+
+// Handle click on the export button
+document.getElementById( 'exportButton' ).addEventListener( 'click', function() {
+    var text = view.state.doc.toString();
+    text = text.replace( /([^\r])\n/g, "$1\r\n" );
+    var blob = new Blob( [text], { type: 'text/plain' }) ;
+    var anchor = document.createElement( 'a' );
+    anchor.download = 'composition.siril';
+    anchor.href = window.URL.createObjectURL( blob );
+    anchor.target = '_blank';
+    anchor.style.display = 'none';
+    document.body.appendChild( anchor );
+    anchor.click();
+    document.body.removeChild (anchor );
 } );
